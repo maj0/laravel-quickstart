76e13e8fc704391c7b9c35a7bb10fd698fdea452
Pi <pi@noreply.smpbx.fritz.box>
2016-11-15 01:59:26 +0100

    initial files

---
+++ public/index.php
@@ -0,0 +1,17 @@
+<?php
+/* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
+
+* File Name : index.php
+
+* Purpose :
+
+* Creation Date : 15-11-2016
+
+* Last Modified : Tue 15 Nov 2016 01:57:49 CET
+
+* Created By : pi
+
+_._._._._._._._._._._._._._._._._._._._._.*/
+
+echo "It works.\n";
+?>
